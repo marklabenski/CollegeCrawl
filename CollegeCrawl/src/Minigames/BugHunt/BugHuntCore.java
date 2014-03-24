4c7ab9e5f17667351edcef782bad2c262ac2dc36
marklabenski <mark.labenski@gmail.com>
2014-03-24 21:55:22 +0100

    Deleted fleet

--- CollegeCrawl/src/Minigames/BugHunt/BugHuntCore.java
+++ CollegeCrawl/src/Minigames/BugHunt/BugHuntCore.java
@@ -1,5 +1,6 @@
 package Minigames.BugHunt;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -20,7 +21,7 @@ public class BugHuntCore extends Minigame{
     @Override
     public void run() {
         super.run();
-        tempSprite.setBounds(100,100,200,200);
+        tempSprite.setBounds(0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
         batch.begin();
 
