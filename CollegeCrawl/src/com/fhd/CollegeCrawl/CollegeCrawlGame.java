fd267c1aa153d94e730c62ee3297de17c24e97f9
marklabenski <swaray@arcor.de>
2014-03-24 19:48:14 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	CollegeCrawl-desktop/src/com/fhd/CollegeCrawl/Main.java

--- CollegeCrawl/src/com/fhd/CollegeCrawl/CollegeCrawlGame.java
+++ CollegeCrawl/src/com/fhd/CollegeCrawl/CollegeCrawlGame.java
@@ -21,6 +21,7 @@ public class CollegeCrawlGame implements ApplicationListener {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 
+
         //testzeile
         //hallo
 
