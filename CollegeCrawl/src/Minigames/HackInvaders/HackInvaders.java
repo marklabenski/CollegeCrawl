4fd7759e2b44d40db64916412df8656a858b74bb
Flo <flo@die-beckerei.de>
2014-03-28 16:28:28 +0100

    HackInvaders
    Grundgerüst für Minigame HackInvaders erstellt

---
+++ CollegeCrawl/src/Minigames/HackInvaders/HackInvaders.java
@@ -0,0 +1,18 @@
+package Minigames.HackInvaders;
+
+import com.fhd.CollegeCrawl.Minigame;
+
+/**
+ * Created by Flo on 28.03.14.
+ */
+public class HackInvaders extends Minigame {
+
+    public HackInvaders() {
+        super();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
