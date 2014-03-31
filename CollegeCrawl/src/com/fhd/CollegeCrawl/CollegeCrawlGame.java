8c3c6aec461b26d1d0a7dece406226bef080e074
Christopherson <chrisv556907@gmail.com>
2014-04-01 00:54:35 +0200

    no message

--- CollegeCrawl/src/com/fhd/CollegeCrawl/CollegeCrawlGame.java
+++ CollegeCrawl/src/com/fhd/CollegeCrawl/CollegeCrawlGame.java
@@ -18,6 +18,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.LocatorEx.Snapshot;
 
 
 public class CollegeCrawlGame implements ApplicationListener
@@ -31,10 +32,14 @@ public class CollegeCrawlGame implements ApplicationListener
 	private Vector2 touch;
 	private ShapeRenderer shaperenderer;
 	private FileManager filemanager;
-	
+
 	private ArrayList<Rectangle> blocks = new ArrayList<Rectangle>();
 	private ArrayList<Animation> animations = new ArrayList<Animation>();
-	
+	private boolean debugmode = true;
+	private Rectangle selected;
+	private Timer buttontimer = new Timer();
+	private Vector2 touchSnaped = new Vector2();
+
 	@Override
 	public void create() {
 		camera = new MaCamera();
@@ -42,17 +47,17 @@ public class CollegeCrawlGame implements ApplicationListener
 		batch = new SpriteBatch();
 		shaperenderer = new ShapeRenderer();
 		filemanager = new FileManager();
-		
+
 		player = new Player();
-		
+
 		worldmap = new Sprite(new Texture("content/map3.png"));
 		worldmap.setBounds(0, 0, 1024, 1024);
 		worldmap.flip(false, true);
-		
-//		blocks.add(new Rectangle(200,200,100,100));
-//		blocks.add(new Rectangle(500,200,100,100));
-//		blocks.add(new Rectangle(200,600,100,100));
-//		blocks.addAll(filemanager.loadBlocks());
+
+		//		blocks.add(new Rectangle(200,200,100,100));
+		//		blocks.add(new Rectangle(500,200,100,100));
+		//		blocks.add(new Rectangle(200,600,100,100));
+		blocks.addAll(filemanager.loadBlocks());
 		animations.add(new Animation(300,300,"temp"));
 	}
 
@@ -71,12 +76,18 @@ public class CollegeCrawlGame implements ApplicationListener
 			camera.update();
 			batch.setProjectionMatrix(camera.combined);
 
-			camera.goHereSmoth(player.getX(), player.getY());
+			if(!debugmode)
+			{
+				camera.goHereSmoth(player.getX(), player.getY());
+				camera.zoom = 0.5f;
+			} else {
+				camera.zoom = 1;
+			}
 
-			batch.begin();
+			batch.begin();////////////////////////////////
 			//draw the map
 			worldmap.draw(batch);
-			
+
 			//draw animations
 			for(Animation a: animations)
 			{
@@ -87,15 +98,19 @@ public class CollegeCrawlGame implements ApplicationListener
 			player.shadow.draw(batch);
 			player.draw(batch);
 			player.update();
-			
-			batch.end();
 
+			batch.end();//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+			collision();
+			editor();
+			if(!debugmode)
+			{
+				input();
+			}		
 		}
-		
-		collision();
-//		editor();
-		input();
-	
+
+
+
 	}
 
 
@@ -129,49 +144,126 @@ public class CollegeCrawlGame implements ApplicationListener
 				player.translateY(-5);	
 				player.setNav(new Vector2(player.getX()+32,player.getY()+50));
 			}
-			*/
+			 */
 		}
 	}
 
 
 	private void editor() {
-		shaperenderer.setProjectionMatrix(camera.combined);
-		shaperenderer.begin(ShapeType.Line);
-
-		shaperenderer.setColor(Color.RED);
-		shaperenderer.rect(player.rect.x, player.rect.y, player.rect.width,player.rect.height);
-		
-//		shaperenderer.setColor(Color.WHITE);
-//		shaperenderer.rect(player.top.x, player.top.y, player.top.width,player.top.height);
-//		shaperenderer.setColor(Color.ORANGE);
-//		shaperenderer.rect(player.left.x, player.left.y, player.left.width,player.left.height);
-//		shaperenderer.setColor(Color.GREEN);
-//		shaperenderer.rect(player.right.x, player.right.y, player.right.width,player.right.height);
-//		shaperenderer.setColor(Color.PINK);
-//		shaperenderer.rect(player.bottom.x, player.bottom.y, player.bottom.width,player.bottom.height);
-
-		shaperenderer.setColor(Color.BLUE);
-		for(Rectangle b : blocks)
+		if(debugmode)
 		{
-			shaperenderer.rect(b.x, b.y, b.width,b.height);
+			if(Gdx.input.justTouched())
+			{
+				//touch coordinates to world coordinates
+				Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+				camera.unproject(pos);
+				touch = new Vector2(pos.x, pos.y);
+
+				for(Rectangle r :blocks)
+				{
+					if(r.contains(touch))
+					{
+						selected = r;
+					}
+				}
+			}
+
+
+
+			if(Gdx.input.isKeyPressed(Keys.SPACE))
+			{
+				//touch coordinates to world coordinates
+				Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+				camera.unproject(pos);
+				touch = new Vector2(pos.x, pos.y);
+
+				camera.goHereSmoth(touch.x, touch.y);
+			} 	
+
+			//wenn ein objekt markiert ist
+			if(selected != null)
+			{
+				if(Gdx.input.isKeyPressed(Keys.W))
+				{
+					//touch coordinates to world coordinates
+					Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+					camera.unproject(pos);
+					touch = new Vector2(pos.x, pos.y);
+
+					touchSnaped.x = touch.x - (touch.x % 32);
+					touchSnaped.y = touch.y - (touch.y % 32);
+					
+					selected.setPosition(touchSnaped);
+				}
+				
+				if(buttontimer.done())
+				{
+					if(Gdx.input.isKeyPressed(Keys.UP))
+					{
+						selected.setHeight(selected.getHeight()-8);
+						buttontimer.wait(100);
+					}
+					if(Gdx.input.isKeyPressed(Keys.DOWN))
+					{
+						selected.setHeight(selected.getHeight()+8);
+						buttontimer.wait(100);
+					}
+					if(Gdx.input.isKeyPressed(Keys.LEFT))
+					{
+						selected.setWidth(selected.getWidth()-8);
+						buttontimer.wait(100);
+					}
+					if(Gdx.input.isKeyPressed(Keys.RIGHT))
+					{
+						selected.setWidth(selected.getWidth()+8);
+						buttontimer.wait(100);
+					}
+				}
+			}
+			shaperenderer.setProjectionMatrix(camera.combined);
+			shaperenderer.begin(ShapeType.Line);
+
+			shaperenderer.setColor(Color.RED);
+			shaperenderer.rect(player.rect.x, player.rect.y, player.rect.width,player.rect.height);
+
+			//		shaperenderer.setColor(Color.WHITE);
+			//		shaperenderer.rect(player.top.x, player.top.y, player.top.width,player.top.height);
+			//		shaperenderer.setColor(Color.ORANGE);
+			//		shaperenderer.rect(player.left.x, player.left.y, player.left.width,player.left.height);
+			//		shaperenderer.setColor(Color.GREEN);
+			//		shaperenderer.rect(player.right.x, player.right.y, player.right.width,player.right.height);
+			//		shaperenderer.setColor(Color.PINK);
+			//		shaperenderer.rect(player.bottom.x, player.bottom.y, player.bottom.width,player.bottom.height);
+
+			for(Rectangle b : blocks)
+			{
+				if(selected != null && b.equals(selected))
+				{
+					shaperenderer.setColor(Color.RED);
+				} else {		
+					shaperenderer.setColor(Color.BLUE);
+				}
+
+				shaperenderer.rect(b.x, b.y, b.width,b.height);
+			}
+
+			shaperenderer.end();
 		}
-		
-		shaperenderer.end();
 	}
 
 
 	private void input() {
-		
+
 		if(Gdx.input.justTouched())
 		{
 			//touch coordinates to world coordinates
 			Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(pos);
 			touch = new Vector2(pos.x, pos.y);
-			
+
 			player.setNav(touch);
 		}
-		
+
 		if(Gdx.input.isKeyPressed(Keys.S))
 		{
 			filemanager.saveBlocks(blocks);
@@ -191,31 +283,7 @@ public class CollegeCrawlGame implements ApplicationListener
 			minigame = null;
 		}
 
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)){
-			player.translateX(2);
-			player.goRight();
-		} else {
-			player.isWalkingRight = false;
-		}
-		if(Gdx.input.isKeyPressed(Keys.LEFT)){
-			player.translateX(-2);
-			player.goLeft();
-		} else {
-			player.isWalkingLeft = false;
-		}
-		
-		if(Gdx.input.isKeyPressed(Keys.UP)){
-			player.translateY(-1.7f);
-			player.goUp();
-		} else {
-			player.isWalkingUp = false;
-		}
-		if(Gdx.input.isKeyPressed(Keys.DOWN)){
-			player.translateY(1.7f);
-			player.goDown();
-		} else {
-			player.isWalkingDown = false;
-		}		
+
 	}
 
 
